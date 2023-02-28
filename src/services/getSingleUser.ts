f4fbd75290b212d2e3d544e9ccf7ed00706b4ec9
ebdem <ebu.bekir.3333@gmail.com>
2023-02-28 15:20:11 +0300

    updated

--- src/services/getSingleUser.ts
+++ src/services/getSingleUser.ts
@@ -3,9 +3,6 @@ import { GRAFBASE_API_KEY, GRAFBASE_API_URL } from "@/config/BASE_URL";
 export const GetSingleUser = async (id: string) => {
   const res = await fetch(GRAFBASE_API_URL as string, {
     method: "POST",
-    next: {
-      revalidate: 1,
-    },
     headers: {
       "content-type": "application/json",
       "x-api-key": GRAFBASE_API_KEY as string,
